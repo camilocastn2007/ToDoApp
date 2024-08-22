44848170f1dbcb1f04eca7d440e24a5fbce6e5b6
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-08-22 11:27:42 -0500

    1 class todo y primera def 2 add tag 3 --strr-- 4 class todobook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -10,10 +10,14 @@ class Todo:
         self.completed = True
 
     def add_tag(self,tag:str):
-        if tag not in self.tags
-          self.tags.append(tag)
+        if tag not in self.tags:
+        self.tags.append(tag)
 
     def __str__(self)-> str:
         return f"{self.code_id}-{self.title}"
+
+class TodoBook:
+    def __init__(self,todos):
+      self.todos: dict[int, todos] = {}
 
 
